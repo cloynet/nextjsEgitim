63d1ef1e708e4177217413487f6ffe8db4b6d7b2
cloynet <eerenn327@hotmail.com>
2025-05-28 16:40:59 +0300

    nextjs

---
+++ my-app/app/head.js
@@ -0,0 +1,11 @@
+export default function Head() {
+    return (
+        <>
+            <title>Blog</title>
+            <meta charSet="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="description" content="Next.js 15 ile sade bir sayfa" />
+            <link rel="icon" href="/favicon.ico" />
+        </>
+    )
+}
---
+++ my-app/components/Footer/index.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+import styles from './styles.module.css';
+
+function Footer() {
+    return <footer className={styles.footer}>
+        <h4>Built with Next.js and love</h4>
+    </footer>
+}
+
+export default Footer;
